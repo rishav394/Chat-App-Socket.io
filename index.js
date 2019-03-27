2a60297f13c95a8a29ebb9672749f83a6fefeae6
Rishav Rungta <rishav394@gmail.com>
2019-03-27 08:43:48 +0530

    Heroku refrag

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var socket = require('socket.io');
 // App setup
 var app = express();
 var port = process.env.PORT || 80;
-var server = app.listen(port, function () {
-    console.log('listening for requests on port ' + port);
+var server = app.listen(port, function() {
+	console.log('listening for requests on port ' + port);
 });
 
 // Online nibas
@@ -13,69 +13,67 @@ var online = [];
 
 // Static files
 app.get('/', (req, res) => {
-    res.render('index.ejs', {
-        count: online.length + 1
-    });
+	res.render('index.ejs', {
+		count: online.length + 1,
+	});
 });
 app.use(express.static('public'));
 
 // Socket setup & pass server
 var io = socket(server);
 io.sockets.on('connection', (socket) => {
+	console.log('Made socket connection ', socket.id);
 
-    console.log('Made socket connection ', socket.id);
+	// Handle user disconnect event
+	socket.on('disconnect', function() {
+		var entry = online.find(function(element) {
+			return String(element.socketId) === socket.id;
+		});
 
-    // Handle user disconnect event
-    socket.on('disconnect', function () {
-        var entry = online.find(function (element) {
-            return String(element.socketId) === socket.id;
-        });
+		// console.log(entry);
+		console.log(socket.id + ' Got disconnected!');
 
-        // console.log(entry);
-        console.log(socket.id + ' Got disconnected!');
+		online = online.filter(function(x) {
+			return x.socketId !== socket.id;
+		});
 
-        online = online.filter(function (x) {
-            return x.socketId !== socket.id;
-        });
+		if (entry !== undefined) {
+			io.sockets.emit('disconnect', [entry.name, online.length]);
+		} else {
+			io.sockets.emit('disconnect', ['a strange user', online.length]);
+		}
+	});
 
-        if (entry !== undefined) {
-            io.sockets.emit('disconnect', [entry.name, online.length]);
-        } else {
-            io.sockets.emit('disconnect', ['a strange user', online.length]);
-        }
-    });
+	// Handle user joined event
+	socket.on('user-joined', function(data) {
+		online.push({
+			socketId: socket.id,
+			name: data.handle,
+		});
+		data.online = online.length;
+		console.log(data);
+		socket.broadcast.emit('user-joined', data);
+	});
 
-    // Handle user joined event
-    socket.on('user-joined', function (data) {
-        online.push({
-            socketId: socket.id,
-            name: data.handle
-        });
-        data.online = online.length;
-        console.log(data);
-        socket.broadcast.emit('user-joined', data);
-    });
+	socket.on('name-change', function(data) {
+		console.log(socket.id + ' requested a name change!');
+		for (var i = 0; i < online.length; i++) {
+			console.log(online[i]);
+			if (String(online[i].socketId) === socket.id) {
+				io.sockets.emit('name-change', [online[i].name, data.handle]);
+				online[i].name = data.handle;
+			}
+		}
+	});
 
-    socket.on('name-change', function (data) {
-        console.log(socket.id + ' requested a name change!');
-        for (var i = 0; i < online.length; i++) {
-            console.log(online[i]);
-            if (String(online[i].socketId) === socket.id) {
-                io.sockets.emit('name-change', [online[i].name, data.handle]);
-                online[i].name = data.handle;
-            }
-        }
-    });
+	// Handle chat event
+	socket.on('chat', function(data) {
+		// console.log(data);
+		io.sockets.emit('chat', data);
+	});
 
-    // Handle chat event
-    socket.on('chat', function (data) {
-        // console.log(data);
-        io.sockets.emit('chat', data);
-    });
-
-    // Handle typing event
-    socket.on('typing', function (data) {
-        socket.broadcast.emit('typing', data);
-    });
-
+	// Handle typing event
+	socket.on('typing', function(data) {
+		socket.broadcast.emit('typing', data);
+	});
 });
--- public/chat.js
+++ public/chat.js
@@ -1,155 +1,186 @@
 // Make connection
+// Choose depending on connection type
 var socket = io.connect('https://rishavchat.herokuapp.com/');
-//var socket = io.connect('127.0.0.1:80');
+// var socket = io.connect('127.0.0.1:80');
 
 // Random number function
-function randomIntFromInterval(min, max) // min and max included
-{
-    return Math.floor(Math.random() * (max - min + 1) + min);
+function randomIntFromInterval(min, max) {
+	// min and max included
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Save cookies to remember username
 function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+	var expires = '';
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+		expires = '; expires=' + date.toUTCString();
+	}
+	document.cookie = name + '=' + (value || '') + expires + '; path=/';
 }
 
 function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
+	var nameEQ = name + '=';
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
 }
 
 function eraseCookie(name) {
-    document.cookie = name + '=; Max-Age=-99999999;';
+	document.cookie = name + '=; Max-Age=-99999999;';
 }
-
 
 // Declaring audio elements
 var audioElement;
 
-$(document).ready(function () {
-    // Query DOM
-    var message = document.getElementById('message'),
-        handle = document.getElementById('handle'),
-        btn = document.getElementById('send'),
-        output = document.getElementById('output'),
-        feedback = document.getElementById('feedback');
+$(document).ready(function() {
+	// Query DOM
+	var message = document.getElementById('message'),
+		handle = document.getElementById('handle'),
+		btn = document.getElementById('send'),
+		output = document.getElementById('output'),
+		feedback = document.getElementById('feedback');
 
-    // Get user handle from the cookie
-    var x = getCookie('handle');
-    if (x) {
-        handle.value = x;
-    } else {
-        handle.value = 'user' + String(randomIntFromInterval(1000, 9999))
-    }
+	// Get user handle from the cookie
+	var x = getCookie('handle');
+	if (x) {
+		handle.value = x;
+	} else {
+		handle.value = 'user' + String(randomIntFromInterval(1000, 9999));
+	}
 
-    socket.emit('user-joined', {
-        handle: handle.value
-    });
+	socket.emit('user-joined', {
+		handle: handle.value,
+	});
 
-    // Set handle name in the cookie
-    $('#handle').focusout(function () {
-        var temp = getCookie('handle');
-        if (temp === handle.value || handle.value === '') {
-            handle.value = temp;
-        } else {
-            // Update in online variable
-            socket.emit('name-change', {
-                handle: handle.value
-            });
-            // Set the cookie for later use
-            setCookie('handle', handle.value, 1);
-        }
-    });
+	// Set handle name in the cookie
+	$('#handle').focusout(function() {
+		var temp = getCookie('handle');
+		if (temp === handle.value || handle.value === '') {
+			handle.value = temp;
+		} else {
+			// Update in online variable
+			socket.emit('name-change', {
+				handle: handle.value,
+			});
+			// Set the cookie for later use
+			setCookie('handle', handle.value, 1);
+		}
+	});
 
-    // Defining audio elements
-    audioElement = document.createElement('audio');
-    audioElement.setAttribute('src', 'https://soundbible.com/mp3/Pling-KevanGC-1485374730.mp3');
+	// Defining audio elements
+	audioElement = document.createElement('audio');
+	audioElement.setAttribute(
+		'src',
+		'https://soundbible.com/mp3/Pling-KevanGC-1485374730.mp3',
+	);
 
-    // Emit events
-    btn.addEventListener('click', function () {
-        if (message.value !== '') {
-            if (handle.value !== '') {
-                socket.emit('chat', {
-                    message: message.value,
-                    handle: handle.value
-                });
-                message.value = "";
-            }
-        } else {
-            // Warn the retarded user
-        }
-    });
+	// Emit events
+	btn.addEventListener('click', function() {
+		if (message.value !== '') {
+			if (handle.value !== '') {
+				socket.emit('chat', {
+					message: message.value,
+					handle: handle.value,
+				});
+				message.value = '';
+			}
+		} else {
+			// Warn the retarded user
+		}
+	});
 
-    message.addEventListener('keypress', function () {
-        socket.emit('typing', handle.value);
-    });
+	message.addEventListener('keypress', function() {
+		socket.emit('typing', handle.value);
+	});
 
-    // Listen for events
-    socket.on('chat', function (data) {
-        audioElement.play();
-        feedback.innerHTML = '';
-        output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
-        $("#chat-window").stop().animate({
-            scrollTop: $("#output")[0].scrollHeight
-        }, 800);
-    });
+	// Listen for events
+	socket.on('chat', function(data) {
+		audioElement.play();
+		feedback.innerHTML = '';
+		output.innerHTML +=
+			'<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+		$('#chat-window')
+			.stop()
+			.animate(
+				{
+					scrollTop: $('#output')[0].scrollHeight,
+				},
+				800,
+			);
+	});
 
-    socket.on('typing', function (data) {
-        feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
-        $("#chat-window").stop().animate({
-            scrollTop: $("#output")[0].scrollHeight
-        }, 800);
-    });
+	socket.on('typing', function(data) {
+		feedback.innerHTML =
+			'<p><em>' + data + ' is typing a message...</em></p>';
+		$('#chat-window')
+			.stop()
+			.animate(
+				{
+					scrollTop: $('#output')[0].scrollHeight,
+				},
+				800,
+			);
+	});
 
-    socket.on('user-joined', function (data) {
-        audioElement.play();
-        $('#onliner').text('Secret Chat (' + data.online + ' online)');
-        output.innerHTML += '<h3>' + data.handle + ' has joined the chat!</h3>';
-        $("#chat-window").stop().animate({
-            scrollTop: $("#output")[0].scrollHeight
-        }, 1000);
-    });
+	socket.on('user-joined', function(data) {
+		audioElement.play();
+		$('#onliner').text('Secret Chat (' + data.online + ' online)');
+		output.innerHTML += '<h3>' + data.handle + ' has joined the chat!</h3>';
+		$('#chat-window')
+			.stop()
+			.animate(
+				{
+					scrollTop: $('#output')[0].scrollHeight,
+				},
+				1000,
+			);
+	});
 
-    socket.on('disconnect', function (data) {
-        audioElement.play();
-        $('#onliner').text('Secret Chat (' + data[1] + ' online)');
-        output.innerHTML += '<h3>' + data[0] + ' has left the chat!</h3>';
-        $("#chat-window").stop().animate({
-            scrollTop: $("#output")[0].scrollHeight
-        }, 1000);
-    });
+	socket.on('disconnect', function(data) {
+		audioElement.play();
+		$('#onliner').text('Secret Chat (' + data[1] + ' online)');
+		output.innerHTML += '<h3>' + data[0] + ' has left the chat!</h3>';
+		$('#chat-window')
+			.stop()
+			.animate(
+				{
+					scrollTop: $('#output')[0].scrollHeight,
+				},
+				1000,
+			);
+	});
 
-    socket.on('name-change', function (data) {
-        output.innerHTML += '<h3>' + data[0] + ' changed their name to ' + data[1] + '</h3>';
-        $("#chat-window").stop().animate({
-            scrollTop: $("#output")[0].scrollHeight
-        }, 1000);
-    });
+	socket.on('name-change', function(data) {
+		output.innerHTML +=
+			'<h3>' + data[0] + ' changed their name to ' + data[1] + '</h3>';
+		$('#chat-window')
+			.stop()
+			.animate(
+				{
+					scrollTop: $('#output')[0].scrollHeight,
+				},
+				1000,
+			);
+	});
 
-    // Enter handlers
-    $('#message').on("keypress", function (e) {
-        if (e.keyCode == 13) {
-            btn.click();
-            return false; // prevent the button click from happening
-        }
-    });
+	// Enter handlers
+	$('#message').on('keypress', function(e) {
+		if (e.keyCode == 13) {
+			btn.click();
+			return false; // prevent the button click from happening
+		}
+	});
 
-    $('#handle').on("keypress", function (e) {
-        if (e.keyCode == 13) {
-            message.focus();
-            return false; // prevent the button click from happening
-        }
-    });
+	$('#handle').on('keypress', function(e) {
+		if (e.keyCode == 13) {
+			message.focus();
+			return false; // prevent the button click from happening
+		}
+	});
 });
